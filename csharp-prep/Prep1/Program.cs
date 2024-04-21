40cecd90e05851bf74c08ec1bd2f84a56f287055
Luciano-A-Vilete <luciano.a.vilete@hotmail.com>
2024-04-21 16:00:10 -0300

    Presentation Code.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.WriteLine("Tell me your first name, please? ");
+        String FirstName = Console.ReadLine();
+
+        Console.WriteLine("Tell me your last name, please? ");
+        String LastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}.");
+
+        
     }
 }
