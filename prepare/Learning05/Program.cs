a893b16dd1beeb00675c61379f7f3d5360ec1dbf
Luciano-A-Vilete <luciano.a.vilete@hotmail.com>
2024-05-22 22:14:04 -0300

    Shape polymorphism activity.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("green", 3);
+        shapes.Add(square1);
+
+        Rectangle rectangle1 = new Rectangle("violet", 3.5, 5.8);
+        shapes.Add(rectangle1);
+
+        Circle circle1 = new Circle("Brown", 5.3);
+        shapes.Add(circle1);
+
+        foreach (Shape s in shapes){
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
